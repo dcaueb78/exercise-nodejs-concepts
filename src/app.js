2553df6b5e973ab9dfe31263167c043f9a906490
Cauê Kotarski <caue1017@hotmail.com>
2020-05-07 23:33:31 -0300

    Complete all routes with TDD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const titleNotExists = !title;
+  const urlNotExists = !url;
+  const techsNotExists = !techs;
+
+  if (titleNotExists || urlNotExists || techsNotExists) {
+    response.status(400).json({ error: "Validation fails" });
+  }
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(newRepository);
+  
+  return response.status(200).json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const selectedRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (selectedRepositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not exits" });
+  }
+
+  const newRepository = {
+    id,
+    title, 
+    url, 
+    techs,
+    likes: repositories[selectedRepositoryIndex].likes
+  }
+  repositories[selectedRepositoryIndex] = newRepository;
+  
+  return response.status(200).json({ ...newRepository });
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const selectedRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if (selectedRepositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not exits" });
+  }
+
+  repositories.splice(selectedRepositoryIndex, 1);
+  
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const selectedRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (selectedRepositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not exits" });
+  }
+
+  repositories[selectedRepositoryIndex].likes++;
+
+  return response.status(200).json({ likes: repositories[selectedRepositoryIndex].likes });
+
 });
 
 module.exports = app;
